2103ebd9f71af017bd3d7ca1cf2bad10cb40e0b9
mohammadsajad Dabbagh <45653004+mdabbagh-ir@users.noreply.github.com>
2022-09-28 23:01:30 +0330

    Update SmsChannelsServiceProvider.php

--- src/SmsChannelsServiceProvider.php
+++ src/SmsChannelsServiceProvider.php
@@ -3,15 +3,15 @@
 namespace Mdabagh\Smschannels;
 
 use Illuminate\Support\ServiceProvider;
+use Mdabagh\Smschannels\Drivers\kavengar;
 
 class SmsChannelsServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind('SmsChannelsInterface', function() {
+        $this->app->bind(SmsChannelsInterface::class, function() {
             return new kavengar;  //return new Smsir;
         });
-
     }
     
     public function boot(){
