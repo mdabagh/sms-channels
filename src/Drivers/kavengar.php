c9e265b39236feff2d8242adcf9103794f9a47ac
mdabagh <mohammadsajaddabbagh@gmail.com>
2023-05-11 09:36:13 +0330

    add drive kavengar

--- src/Drivers/kavengar.php
+++ src/Drivers/kavengar.php
@@ -9,9 +9,34 @@ class kavengar implements SmsChannelsInterface
 {
     public function sendVerifyCode($phone)
     {
-        return ['kavengar'];
+        $url = "https://api.kavenegar.com/v1/" . config('sms.kavenegar.api_key') . "/verify/lookup.json";
+        $params = [
+            'receptor' => $phone,
+            'token' => config('sms.kavenegar.token'),
+            'template' => config('sms.kavenegar.template'),
+            'type' => 'sms'
+        ];
+        if (config('sms.kavenegar.token2')) {
+            $params['token2'] = config('sms.kavenegar.token2');
+        }
+        if (config('sms.kavenegar.token3')) {
+            $params['token3'] = config('sms.kavenegar.token3');
+        }
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'charset' => 'utf-8'
+        ])->get($url, $params);
+
+        $body = $response->json();
+        if ($body['return']['status'] == 200) {
+            return $body['entries'][0];
+        } else {
+            return false;
+        }
     }
-    public function checkVerifyCode($phone , $key)
+
+    public function checkVerifyCode($phone, $key)
     {
         return ['kavengar'];
     }
--- src/config/sms.php
+++ src/config/sms.php
@@ -7,5 +7,14 @@ return [
         'authentication' =>env('MRAPI_AUTHENTICATION'),
         'token' =>env('MRAPI_TOKEN'),
         'patternid' =>env('MRAPI_PATTERNID'),
+    ],
+
+    'kavenegar' => [
+        'api_key' => env('KAVENEGAR_API_KEY'),
+        'token' => env('KAVENEGAR_TOKEN', null),
+        'token2' => env('KAVENEGAR_TOKEN2', null),
+        'token3' => env('KAVENEGAR_TOKEN3', null),
     ]
+
+
 ];
